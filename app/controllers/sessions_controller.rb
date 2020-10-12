b5e8b90261ed91bef22487d57e033a3c828dcfd3
hiromu <shinji06011@gmail.com>
2020-10-12 08:28:38 +0000

    Add account activation

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,6 @@ class SessionsController < ApplicationController
   
   #ログインページ表示
   def new
-    
   end
   
   #ログイン
@@ -11,10 +10,17 @@ class SessionsController < ApplicationController
     user = User.find_by(email:params[:session][:email].downcase)
     #入力されたユーザーがDBに存在し、かつ認証に成功か：失敗したらレンダリングしない
     if user && user&.authenticate(params[:session][:password])
-      log_in user
-      #chek_box ON:1 OFF:0
-      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-      redirect_back_or user
+      if user.activated?
+        log_in user
+         #chek_box ON:1 OFF:0
+        params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+        redirect_back_or user
+      else
+        message  = "Account not activated. "
+        message += "Check your email for the activation link."
+        flash[:warning] = message
+        redirect_to root_url
+      end
     else
       flash.now[:danger] = "Invalid email/password combination"
       render 'new' 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token
+  attr_accessor :remember_token,:activation_token
   #保存される前に実行されるアクション
-  before_save { self.email = email.downcase }
+  before_save :downcase_email
+  before_create :create_activation_digest
   
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   #存在チェック,文字数チェック
@@ -12,6 +13,16 @@ class User < ApplicationRecord
   validates :password, presence: true, length:{minimum: 6}, allow_nil: true
   
   has_secure_password
+  
+   # アカウントを有効にする
+  def activate
+    update_columns(activated: true, activated_at: Time.zone.now)
+  end
+
+  # 有効化用のメールを送信する
+  def send_activation_email
+    UserMailer.account_activation(self).deliver_now
+  end
   
   #渡された文字列のハッシュ値を返す
   def User.digest(string)
@@ -30,14 +41,27 @@ class User < ApplicationRecord
     self.remember_token = update_attribute(:remember_digest, User.digest(remember_token))
   end
   
-  # 渡されたトークンがダイジェストと一致したらtrueを返す
-  def authenticated?(remember_token)
-    return false if remember_digest.nil?
-    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  # トークンがダイジェストと一致したらtrueを返す
+  def authenticated?(attribute, token)
+    digest = send("#{attribute}_token")
+    return false if digest.nil?
+    BCrypt::Password.new(attribute).is_password?(token)
   end
   
   # ユーザーのログイン情報を破棄する
   def forget
     update_attribute(:remember_digest, nil)
   end
+  
+  private
+    # メールアドレスをすべて小文字にする
+    def downcase_email
+      self.email.downcase!
+    end
+    
+    # 有効化トークンとダイジェストを作成および代入する
+    def create_activation_digest
+      self.activation_token = User.new_token
+      self.activation_token = User.digest(activation_token)
+    end
 end
